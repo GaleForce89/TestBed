14c84cd546d396cc4f174402d36161932d38d0b2
GaleForce <GaleForce089@gmail.com>
2019-08-17 13:51:13 -0500

    day11 start

--- Day11- 2d Arrays/Program.cs
+++ Day11- 2d Arrays/Program.cs
@@ -1,12 +1,29 @@
-﻿using System;
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
 
-namespace Day11__2d_Arrays
+class Solution
 {
-    class Program
+
+
+
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        int[][] arr = new int[6][];
+
+        for (int i = 0; i < 6; i++)
         {
-            Console.WriteLine("Hello World!");
+            arr[i] = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp));
         }
     }
 }
