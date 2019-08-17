57aba0af8f55cae5a7be89f4638874ba9f969350
GaleForce <GaleForce089@gmail.com>
2019-08-17 12:45:41 -0500

    loops done

---
+++ Loops/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution
+{
+
+
+
+    static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine());
+        for(int x = 1;x <= 10; x++)
+        {
+            int result = n * x;
+            Console.WriteLine("{0} x {1} = {2}",n, x, result);
+        }
+    }
+}
