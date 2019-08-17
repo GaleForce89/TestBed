3c9893bbe7f520568d784fcf2a931d7182a4a7f5
GaleForce <GaleForce089@gmail.com>
2019-08-17 12:30:12 -0500

    class vs instance completed

--- Class vs Instance/Program.cs
+++ Class vs Instance/Program.cs
@@ -12,14 +12,62 @@ If  >=13 and <18, print You are a teenager..
 Otherwise, print You are old..
 */
 using System;
+using System.Collections.Generic;
+using System.IO;
 
-namespace Class_vs_Instance
+class Person
 {
-    class Program
+    public int age;
+    public Person(int initialAge)
     {
-        static void Main(string[] args)
+        // Add some more code to run some checks on initialAge
+        if(initialAge < 0)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Age is not valid, setting age to 0.");
+            this.age = 0;
+        }
+        else
+        {
+            this.age = initialAge;
+        }
+    }
+    public void amIOld()
+    {
+        // Do some computations in here and print out the correct statement to the console
+        if(this.age < 13)
+        {
+            Console.WriteLine("You are young.");
+        }
+        else if(this.age >= 18)
+        {
+            Console.WriteLine("You are old.");
+        }
+        else
+        {
+            Console.WriteLine("You are a teenager.");
+        }
+    }
+
+    public void yearPasses()
+    {
+        // Increment the age of the person in here
+        this.age++;
+    }
+
+    static void Main(String[] args)
+    {
+        int T = int.Parse(Console.In.ReadLine());
+        for (int i = 0; i < T; i++)
+        {
+            int age = int.Parse(Console.In.ReadLine());
+            Person p = new Person(age);
+            p.amIOld();
+            for (int j = 0; j < 3; j++)
+            {
+                p.yearPasses();
+            }
+            p.amIOld();
+            Console.WriteLine();
         }
     }
 }
