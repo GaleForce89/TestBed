a0f2a151e0e176544e861d3e7060bd9cd945428e
GaleForce <GaleForce089@gmail.com>
2019-08-27 22:43:22 -0500

    Finished a few days

---
+++ Day 16 - Exceptions - String to Integer/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class Solution
+{
+
+    static void Main(String[] args)
+    {
+        string S = Console.ReadLine();
+        try
+        {
+            Console.WriteLine(Int32.Parse(S));
+        }
+        catch
+        {
+            Console.WriteLine("Bad String");
+        }
+    }
+}
+
