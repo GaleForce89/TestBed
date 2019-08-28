520fe9543e8c710d121edbea268cf7718269dd19
GaleForce <GaleForce089@gmail.com>
2019-08-27 23:19:50 -0500

    day 22

---
+++ Day 22 - Binary Search Trees/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Day_22___Binary_Search_Trees
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
